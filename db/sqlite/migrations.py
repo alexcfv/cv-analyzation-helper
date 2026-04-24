7c998caf484e21d4af0d45cda42f9cd1d3345077
Kramskoi Alexey <kramskoialexej@gmail.com>
2026-04-24 18:58:36 +0200

    add initial db migration

---
+++ db/sqlite/migrations.py
@@ -0,0 +1,10 @@
+from db.connection import get_db
+
+def init_db():
+    with get_db() as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS profiles (
+                id TEXT PRIMARY KEY,
+                profile JSON
+            )
+        """)
